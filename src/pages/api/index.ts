a5392aca657d07b1c1e816aaa717589d7ca5337e
Jinna Chodchoy <jinna.chodchoy@agoda.com>
2021-09-23 12:56:29 +0700

    fix(api/index.ts): increase upload size

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -5,8 +5,14 @@ import { connectDB } from './repository'
 import LineMiddleware from './middleware/line'
 
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '200mb' }))
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '200mb',
+    parameterLimit: 200000,
+  }),
+)
 
 app.use(async (req, res, next) => {
   await connectDB()
