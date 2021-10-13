637fca755874a87aec4786f665abcae4c56bb667
Jinna Chodchoy <jinna.chodchoy@agoda.com>
2021-10-13 23:44:48 +0700

    fix: fix toast show multiple same error

--- src/components/AxiosErrorToast.tsx
+++ src/components/AxiosErrorToast.tsx
@@ -1,23 +1,30 @@
 import backendInstance from '@/lib/axios'
 import { createStandaloneToast } from '@chakra-ui/react'
 import { AxiosError } from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useCallback, useState } from 'react'
 
 const AxiosErrorToast = () => {
+  const [error, setError] = useState<AxiosError>()
   const toast = createStandaloneToast()
+
+  useEffect(() => {
+    if (error) {
+      toast({
+        title: 'An error occurred.',
+        description: JSON.stringify(error.message),
+        status: 'error',
+        duration: null,
+        isClosable: true,
+      })
+    }
+  }, [error])
 
   useEffect(() => {
     backendInstance.interceptors.response.use(
       (response) => response,
       (error: AxiosError) => {
         if (error && error?.response?.status) {
-          toast({
-            title: 'An error occurred.',
-            description: JSON.stringify(error.message),
-            status: 'error',
-            duration: null,
-            isClosable: true,
-          })
+          setError(error)
         }
         return Promise.reject(error)
       },
