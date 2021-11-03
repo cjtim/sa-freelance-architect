8a793da13617db3fdcebc24f88015af771dbafde
Jinna Chodchoy <jinna.chodchoy@agoda.com>
2021-11-03 15:29:06 +0700

    feat: add btn to copy project furniture for customer

--- src/pages/projects/[id]/furnitures.tsx
+++ src/pages/projects/[id]/furnitures.tsx
@@ -6,12 +6,18 @@ import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import { fetchFurnitures } from '@/slices/funiture'
 import ProjectFurnitureForm from '@/components/projects/ProjectFurnitureForm'
-import { Container } from '@chakra-ui/react'
+import { Button, Container, useClipboard } from '@chakra-ui/react'
+import NavBar from '@/components/NavBar'
+import { CopyIcon } from '@chakra-ui/icons'
+import { hostname } from '@/utils/hostname'
 
 const ProjectFurniturePage = () => {
   const dispatch = useAppDispatch()
   const router = useRouter()
   const { id } = router.query
+  const { hasCopied, onCopy } = useClipboard(
+    `${hostname}/customer/furnitures/${id}`,
+  )
 
   useEffect(() => {
     dispatch(fetchProjectFurnitures(Number(id)))
@@ -21,6 +27,12 @@ const ProjectFurniturePage = () => {
   return (
     <PageLayout windowTitle="Contract">
       <Container maxW="container.xl">
+        <NavBar />
+
+        <Button onClick={onCopy} my={4}>
+          <CopyIcon />
+          {hasCopied ? 'Copied' : 'คัดลอกลิงค์ สำหรับลูกค้าเลือกเฟอร์นิเจอร์'}
+        </Button>
         <ProjectFurnitureForm project_id={Number(id)} />
       </Container>
     </PageLayout>
